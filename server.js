c71e2434d6cb73171f0812b8960a6fe20f47f9a8
CedricA <cedric.j.aoun@gmail.com>
2025-06-16 14:52:02 +0200

    added /date route to display date and time

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/date', (req, res) => {
+  res.send(`Current date and time: ${new Date().toISOString()}`);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
