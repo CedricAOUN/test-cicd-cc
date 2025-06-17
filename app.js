937f7df6c12b81642b00ca61ec71cac85e951d45
CedricA <cedric.j.aoun@gmail.com>
2025-06-17 15:18:29 +0200

    Add /version route and tests

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get('/status', (req, res) => {
   res.send({'status': 'API is running'});
 });
 
+app.get('/version', (req, res) => {
+  res.send({'version': '1.0.1'});
+});
+
 // Only start server if this file is run directly
 // Used to prevent server starting when imported in tests
 if (require.main === module) {
